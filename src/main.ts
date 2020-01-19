71ce6d7fb28b95e8ea7c0e013e78c5002bee5c14
Alexandre <acharlot91@gmail.com>
2020-01-19 23:15:27 +0100

    Update test.yml

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,11 @@ import * as github from '@actions/github'
 import * as core from '@actions/core'
 
 async function run(): Promise<void> {
-  const myToken = core.getInput('myToken')
+  const myToken = core.getInput('github-token')
 
   const octokit = new github.GitHub(myToken)
 
-  const foo = octokit.repos.listBranches({
+  const foo = await octokit.repos.listBranches({
     owner: github.context.repo.owner,
     repo: github.context.repo.repo
   })

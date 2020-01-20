491018144b1119ed8be01e250abfcde7665091eb
Alexandre Charlot <acharlot91@gmail.com>
2020-01-20 10:23:36 +0100

    make a modification

--- index.js
+++ index.js
@@ -106,8 +106,8 @@ async function run() {
   const fetchedTags = await tags()
   const commits = await getCommits()
   const { newVersion } = bump(fetchedTags, commits)
-  console.log(fetchedTags)
   console.log(newVersion)
+  console.log(fetchedTags)
   const sha = await updatePackageJson(newVersion, commits)
   const { data } = await octokit.git.createTag({
     ...settings,

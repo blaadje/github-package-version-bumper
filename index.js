e1d918cda2d1b9c2f1512f9d688cc03729d92e6f
Alexandre <acharlot91@gmail.com>
2020-05-23 17:03:57 +0200

    fix: fisuenf

--- index.js
+++ index.js
@@ -91,7 +91,6 @@ async function updatePackageJson(newVersion, currentVersion) {
     const content = Buffer.from(JSON.stringify(updatedFile, null, 2)).toString(
       'base64'
     )
-    console.log('test')
 
     try {
       const { data } = await octokit.repos.createOrUpdateFile({

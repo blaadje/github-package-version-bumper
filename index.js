cb3ac5c5601fa4d101740656c37b23fa602e1429
Alexandre <acharlot91@gmail.com>
2022-05-04 11:59:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -130,17 +130,10 @@ async function run() {
       type: 'commit'
     })
 
-    await octokit.git.createRef({
+    octokit.git.createRef({
       ...settings,
       ref: `refs/tags/v${newVersion}`,
       sha: data.sha
-    })
-
-    await octokit.repos.createRelease({
-      ...settings,
-      tag_name: `v${newVersion}`,
-      name,
-      draft: true
     })
   } catch (error) {
     core.setFailed(error.message)
